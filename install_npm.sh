08fb07e0c4caa65e330eaa39b9148d49ebcddb65
李兴华 <lixinghua@lixinghuadeMBP.lan>
2019-12-29 22:13:56 +0800

    add:npm hexo ...

---
+++ install_npm.sh
@@ -0,0 +1,6 @@
+npm install -g hexo-cli
+npm install hexo-deployer-git --save
+npm install hexo-generator-feed --save
+npm un hexo-renderer-marked --save
+npm i hexo-renderer-kramed --save
+npm install hexo-generator-searchdb --save
